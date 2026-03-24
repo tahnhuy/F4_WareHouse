d9041e8cb1671d5916d1d7aac01b097175654bea
Nhat Huy <tahnhuy25@gmail.com>
2026-03-24 23:48:46 +0000

    func: fix customer controller

--- server/src/controllers/customer.controller.ts
+++ server/src/controllers/customer.controller.ts
@@ -1,55 +1,22 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import { CustomerService } from "../services/customer.service";
 
 export class CustomerController {
-  static async getAllCustomers(req: Request, res: Response, next: NextFunction) {
+  static async getAllCustomers(req: Request, res: Response) {
     try {
       const customers = await CustomerService.getAllCustomers();
-      res.status(200).json({ success: true, data: customers });
-    } catch (error) {
-      next(error);
+      return res.status(200).json({ success: true, data: customers });
+    } catch (error: any) {
+      return res.status(400).json({ success: false, message: error.message });
     }
   }
 
-  static async getCustomerById(req: Request, res: Response, next: NextFunction) {
-    try {
-      const id = parseInt(req.params.id);
-      const customer = await CustomerService.getCustomerById(id);
-      if (!customer) {
-        return res.status(404).json({ success: false, message: "Không tìm thấy khách hàng" });
-      }
-      res.status(200).json({ success: true, data: customer });
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  static async createCustomer(req: Request, res: Response, next: NextFunction) {
+  static async createCustomer(req: Request, res: Response) {
     try {
       const customer = await CustomerService.createCustomer(req.body);
-      res.status(201).json({ success: true, data: customer });
+      return res.status(201).json({ success: true, data: customer });
     } catch (error: any) {
-      res.status(400).json({ success: false, message: error.message });
-    }
-  }
-
-  static async updateCustomer(req: Request, res: Response, next: NextFunction) {
-    try {
-      const id = parseInt(req.params.id);
-      const customer = await CustomerService.updateCustomer(id, req.body);
-      res.status(200).json({ success: true, data: customer });
-    } catch (error: any) {
-      res.status(400).json({ success: false, message: error.message });
-    }
-  }
-
-  static async deleteCustomer(req: Request, res: Response, next: NextFunction) {
-    try {
-      const id = parseInt(req.params.id);
-      await CustomerService.deleteCustomer(id);
-      res.status(200).json({ success: true, message: "Xoá khách hàng thành công" });
-    } catch (error: any) {
-      res.status(400).json({ success: false, message: error.message });
+      return res.status(400).json({ success: false, message: error.message });
     }
   }
 }
